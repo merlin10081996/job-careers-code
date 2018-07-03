d3fbaa62a8e5b065a380aed0c79d0974c0cb96b7
Naveen S <snsknaveensaravanan@gmail.com>
2018-07-03 14:50:05 +0000

    Resume Page

--- job-careers-react/src/components/Resume.jsx
+++ job-careers-react/src/components/Resume.jsx
@@ -14,26 +14,28 @@ class Resume extends Component {
     render() {
         return (
             <form>
-                <div className="form-group">
-                    <img src={vzLogo} width={100} height={5} alt="..." className="img-thumbnail" /></div>
-
 
                 <div>
-                    <label>First Name      </label>
-                    <input type="text"  id="usr"/>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>First Name:</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="text"  id="usr" className="form-control" id="sel1" class ="col-sm-2"/>
 
                 </div>
                 <div>
-                    <label>Last Name</label>
-                    <input type="text"  id="usr"/>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Last Name:</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="text"  id="usr" className="form-control" id="sel1" class ="col-sm-2"/>
                 </div>
                 <div>
-                    <label>Email</label>
-                    <input type="text"  id="usr"/>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Email</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="text"  id="usr" className="form-control" id="sel1" class ="col-sm-2"/>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;@</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <select className="form-control" id="sel1" className="col-sm-2">
+                        <option>gmail.com</option>
+                        <option>yahoo.com</option>
+                    </select>
                 </div>
                 <div>
-                    <label htmlFor="sel1">Country</label>
-                    <select className="form-control" id="sel1">
+                    <label htmlFor="sel1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Country</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <select className="form-control" id="sel1" class ="col-sm-2">
                         <option>Ind</option>
                         <option>US</option>
                         <option>China</option>
@@ -41,16 +43,16 @@ class Resume extends Component {
                     </select>
                 </div>
                 <div>
-                    <label>Zip Code</label>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Zip Code</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <input type="text"  id="usr"/>
                 </div>
                 <div>
-                    <label>City</label>
-                    <input type="text"  id="usr"/>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>City</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="text"  id="usr" className="form-control" id="sel1" class ="col-sm-2"/>
                 </div>
                 <div>
-                    <label htmlFor="sel1">State</label>
-                    <select className="form-control" id="sel1">
+                    <label htmlFor="sel1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>State</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <select className="form-control" id="sel1" className="form-control" id="sel1" class ="col-sm-2">
                         <option>Ind</option>
                         <option>US</option>
                         <option>China</option>
@@ -58,71 +60,109 @@ class Resume extends Component {
                     </select>
                 </div>
                 <div>
-                    <label htmlFor="sel1">Work Experience</label>
-                    <select className="form-control" id="sel1">
+                    <label htmlFor="sel1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Work Experience</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <select className="form-control" id="sel1" className="form-control" id="sel1" class ="col-sm-2">
                         <option>Yes</option>
                         <option>No</option>
                     </select>
                 </div>
                 <div>
-                    <label>Employer</label>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Employer</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <input type="text"  id="usr"/>
                 </div>
                 <div>
-                    <label>Job Title</label>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Job Title</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <input type="text"  id="usr"/>
                 </div>
                 <div>
-                    <label htmlFor="sel1">Start Year</label>
-                    <select className="form-control" id="sel1">
-                        <option>Yes</option>
-                        <option>No</option>
+                    <label htmlFor="sel1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Start Year</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <select className="form-control" id="sel1" className="form-control" id="sel1" class ="col-sm-2">
+                        <option>2018</option>
+                        <option>2017</option>
+                        <option>2016</option>
+                        <option>2015</option>
+                        <option>2014</option>
+                        <option>2013</option>
+                        <option>2012</option>
+                        <option>2011</option>
+                        <option>2010</option>
+                        <option>2009</option>
+                        <option>2008</option>
+                        <option>2007</option>
                     </select>
                 </div>
                 <div>
-                    <label htmlFor="sel1">Start Year</label>
-                    <select className="form-control" id="sel1">
-                        <option>Yes</option>
-                        <option>No</option>
+                    <label htmlFor="sel1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Start Month</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <select className="form-control" id="sel1" className="form-control" id="sel1" class ="col-sm-2">
+                        <option>January</option>
+                        <option>February</option>
+                        <option>March</option>
+                        <option>April</option>
+                        <option>May</option>
+                        <option>June</option>
+                        <option>July</option>
+                        <option>August</option>
+                        <option>September</option>
+                        <option>October</option>
+                        <option>November</option>
+                        <option>December</option>
                     </select>
-                </div>ss
+                </div>
                 <div>
-                    <label>End Month</label>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>End Month</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <select className="form-control" id="sel1" className="form-control" id="sel1" className="col-sm-2">
+                        <option>January</option>
+                        <option>February</option>
+                        <option>March</option>
+                        <option>April</option>
+                        <option>May</option>
+                        <option>June</option>
+                        <option>July</option>
+                        <option>August</option>
+                        <option>September</option>
+                        <option>October</option>
+                        <option>November</option>
+                        <option>December</option>
+                    </select>
+                </div>
+                <div>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>End Year</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <select className="form-control" id="sel1" className="form-control" id="sel1" className="col-sm-2">
+                        <option>2018</option>
+                        <option>2017</option>
+                        <option>2016</option>
+                        <option>2015</option>
+                        <option>2014</option>
+                        <option>2013</option>
+                        <option>2012</option>
+                        <option>2011</option>
+                        <option>2010</option>
+                        <option>2009</option>
+                        <option>2008</option>
+                        <option>2007</option>
+                    </select>
+                </div>
+                <div>
+                    <label for="comment">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Responbilities</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <textarea className="form-control" rows="5" id="comment" className="col-sm-2"></textarea>
+                </div>
+                <div>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Education</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <input type="text"  id="usr"/>
                 </div>
                 <div>
-                    <label>End Year</label>
-                    <input type="text"  id="usr"/>
+                    <label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Degree Status</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <input type="text "  id="usr"/>
                 </div>
                 <div>
-                    <label>Responbilities</label>
-                    <input type="text"  id="usr"/>
+                    <label htmlFor="comment">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<b>Additional Info</b></label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <textarea className="form-control" rows="5" id="comment" className="col-sm-2"></textarea>
                 </div>
                 <div>
-                    <button type="submit" className="btn btn-default"><b>Additional Information</b></button>
-                </div>
-                <div>
-                    <label>Country</label>
-                    <input type="text"  id="usr"/>
-                </div>
-                <div>
-                    <label>Education</label>
-                    <input type="text"  id="usr"/>
-                </div>
-                <div>
-                    <label>Degree Status</label>
-                    <input type="text"  id="usr"/>
-                </div>
-                <div>
-                    <button type="submit" className="btn btn-default"><b>Additional Information</b></button>
-                </div>
-                <div>
-                    <button type="submit" className="btn btn-default"><b>Save As Draft</b></button>
-                </div>
-                <div><button type="submit" className="btn btn-default"><NavLink to="/ApplyButton">Back </NavLink></button>
-                    <Route exact path="/ApplyButton" component={ApplyButton}/></div>
-                <div>
-                    <button type="submit" className="btn btn-default"><b>Submit</b></button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button type="submit" className="btn btn-default"><NavLink to="/ApplyButton">Back </NavLink></button>
+                    <Route exact path="/ApplyButton" component={ApplyButton}/>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <button type="submit" className="btn btn-default"><b>Save As Draft</b></button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button type="submit" className="btn btn-default"><b>Submit</b></button>
                 </div>
 
             </form>
